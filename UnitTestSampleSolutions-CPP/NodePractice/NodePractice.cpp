1e6515f8a56eff16df0fe40d2d88764627c07980
Branko Zikic <branko.zikic@student.forsbergsskola.se>
2024-05-17 14:35:21 +0200

    Added insert at the front and the end function

--- UnitTestSampleSolutions-CPP/NodePractice/NodePractice.cpp
+++ UnitTestSampleSolutions-CPP/NodePractice/NodePractice.cpp
@@ -16,6 +16,31 @@ void printList(Node* node)
         node = node->next;
     }
 }
+void insertAtTheFront(Node** headAddress, int newValue)
+{
+    Node* newNode = new Node();
+    newNode->value = newValue;
+
+    newNode->next = *headAddress;
+
+    *headAddress = newNode;
+}
+void insertAtTheEnde(Node** headAddress, int newValue)
+{
+    Node* newNode = new Node();
+    newNode->value = newValue;
+    newNode->next = nullptr;
+
+    if (*headAddress == nullptr)
+        *headAddress = newNode;
+
+    Node* last = *headAddress;
+    while (last->next != nullptr)
+    {
+        last = last->next;
+    }
+    last->next = newNode;
+}
 
 int main()
 {
@@ -32,5 +57,7 @@ int main()
     third->value = 3;
     third->next = nullptr;
 
+    insertAtTheFront(&head, -1);
+    insertAtTheFront(&head, -2);
     printList(head);
 }
