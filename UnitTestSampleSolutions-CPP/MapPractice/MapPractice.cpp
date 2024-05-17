80c165d60dab06a63bee2ae16ad2d4110a5b3655
Branko Zikic <branko.zikic@student.forsbergsskola.se>
2024-05-17 16:06:21 +0200

    Adding functions

--- UnitTestSampleSolutions-CPP/MapPractice/MapPractice.cpp
+++ UnitTestSampleSolutions-CPP/MapPractice/MapPractice.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <map>      //unordered_map is another map but its not ordered .. duhh
 
 using namespace std;
 
@@ -13,6 +13,12 @@ int main()
     myDictionary.insert(pair<string,string>("banana","banana"));
     myDictionary.insert(pair<string,string>("plant","biljka, saditi"));
 
+    myDictionary["plant"] = "Biljka, Saditi";
+
+    myDictionary.clear();
+
+    cout << "Size of Dictionary is: " << myDictionary.size() << endl; 
+    
     for (auto pair : myDictionary)
     {
         //map will always organize the list in the alphabetic order (ascending order) - a,b,c,d ... 1,2,3,4
