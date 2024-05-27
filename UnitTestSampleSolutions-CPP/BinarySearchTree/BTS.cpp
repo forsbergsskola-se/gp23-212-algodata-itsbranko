3f16060d2d9fbcac8fff447e01055cac78d894f4
Branko Zikic <branko.zikic@student.forsbergsskola.se>
2024-05-27 16:42:18 +0200

    Spliting the header and cpp file

--- UnitTestSampleSolutions-CPP/BinarySearchTree/BTS.cpp
+++ UnitTestSampleSolutions-CPP/BinarySearchTree/BTS.cpp
@@ -1,1 +1,14 @@
 ﻿#include "BTS.h"
+
+Node* CreateNode(int value)
+{
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+void Insert(int value)
+{
+    BTS::root = 
+}
--- UnitTestSampleSolutions-CPP/BinarySearchTree/BTS.h
+++ UnitTestSampleSolutions-CPP/BinarySearchTree/BTS.h
@@ -1,7 +1,21 @@
 ﻿#pragma once
+#include <stack>
+
+struct  Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+
+Node* CreateNode(int value);
+void Insert(int value);
+Node* insertRec(Node* node, int val);
 
 class BTS
 {
 public:
+    Node* root = CreateNode(10);
     
 };
